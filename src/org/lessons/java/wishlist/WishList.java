52332b576cf6c73c960d34a29521eb951164a206
GiovanniLafortezza <recordeknot388@gmail.com>
2023-12-07 14:51:20 +0100

    user add the gifts

--- src/org/lessons/java/wishlist/WishList.java
+++ src/org/lessons/java/wishlist/WishList.java
@@ -11,5 +11,34 @@ public class WishList {
         HashSet<String> wishList = new HashSet<>();
         System.out.println(wishList.size());
 
+        boolean stop = false;
+        //chiediamo all'utente di inserire un regalo alla volta
+        while(!stop) {
+            System.out.println("Vuoi inseire un nuovo regalo? (y/n)");
+            String answer = scanner.nextLine();
+            switch (answer){
+                case  "y":
+                    //aggiungo il regalo
+                    System.out.println("Nome regalo: ");
+                    String gift = scanner.nextLine();
+                    boolean added = wishList.add(gift);
+                    //se il regalo è già stato aggiunto
+                    if(!added) {
+                        System.out.println("Mi dispiace ma questo è già stato inserito");
+                    }
+                    break;
+                case "n":
+                    //esco
+                    System.out.println("ok");
+                    stop = true;
+                    break;
+                default:
+                    //risposta sbagliata
+                    System.out.println("Risposta non valida");
+                    break;
+
+            }
+        }
+
     }
 }
