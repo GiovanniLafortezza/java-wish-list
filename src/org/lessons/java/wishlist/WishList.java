3a3844198129af4894d84bdf8b3d83ada690e596
GiovanniLafortezza <recordeknot388@gmail.com>
2023-12-07 14:34:55 +0100

    create the wish list empty

--- src/org/lessons/java/wishlist/WishList.java
+++ src/org/lessons/java/wishlist/WishList.java
@@ -1,7 +1,15 @@
 package org.lessons.java.wishlist;
 
+import java.util.HashSet;
+import java.util.Scanner;
+
 public class WishList {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //creiamo la lista di regali vuota
+        HashSet<String> wishList = new HashSet<>();
+        System.out.println(wishList.size());
 
     }
 }
